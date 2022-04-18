aae7d58f214ed845f7d35d243e888a97b4ed92ab
abhishek <abbhishekk27@gmail.com>
2022-04-18 10:26:40 +0530

    completed interest implementation

--- src/screens/FriendsScreen.js
+++ src/screens/FriendsScreen.js
@@ -1,20 +1,39 @@
-import {Box, Center, Container, Text, Flex} from 'native-base';
+import {Box, Center, Container, Text, Flex, Button} from 'native-base';
 import React, {useState, useEffect} from 'react';
 import FriendReq from '../../components/FriendReq';
 import SearchInput from '../../components/SearchInput';
 import SelectBox from 'react-native-multi-selectbox';
 import {xorBy} from 'lodash';
-import {listInterests} from '../graphql/queries';
+import {listInterests, getUser} from '../graphql/queries';
+import {createUserInterests} from '../graphql/mutations';
 import {Auth, API, graphqlOperation} from 'aws-amplify';
 import {Alert} from 'react-native';
 
 export default function FriendsScreen() {
   const [interests, setInterests] = useState([]);
   const [selectedInterests, setSelectedInterests] = useState([]);
+  const [userID, setUserID] = useState('');
 
   function onMultiChange() {
+    console.log(selectedInterests);
     return item => setSelectedInterests(xorBy(selectedInterests, [item], 'id'));
   }
+
+  const getUserData = async () => {
+    const currentUser = await Auth.currentAuthenticatedUser();
+    try {
+      const currentUserData = await API.graphql(
+        graphqlOperation(getUser, {id: currentUser.attributes.sub}),
+      );
+      const currentUserInfo = currentUserData.data.getUser.id;
+
+      if (currentUser) {
+        setUserID(currentUserInfo);
+      }
+    } catch (e) {
+      Alert.alert(e.message);
+    }
+  };
 
   const getAllInterests = async () => {
     try {
@@ -27,8 +46,6 @@ export default function FriendsScreen() {
         }));
 
         setInterests(interestmap);
-
-        console.log(interests);
       }
     } catch (e) {
       Alert.alert(e.message);
@@ -38,6 +55,27 @@ export default function FriendsScreen() {
   useEffect(() => {
     getAllInterests();
   }, []);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const submitInterest = async () => {
+    for (let i = 0; i < selectedInterests.length; i++) {
+      try {
+        await API.graphql(
+          graphqlOperation(createUserInterests, {
+            input: {
+              userID: userID,
+              interestID: selectedInterests[i].id,
+            },
+          }),
+        );
+      } catch (e) {
+        // Alert.alert(e.message);
+      }
+    }
+  };
 
   return (
     <Box w="100%" m="0" p="0" flex={1} backgroundColor="#fff">
@@ -51,16 +89,24 @@ export default function FriendsScreen() {
       <Center>
         <SelectBox
           width="80%"
-          label="Select multiple"
+          label="Select interests"
           options={interests}
           selectedValues={selectedInterests}
           onMultiSelect={onMultiChange()}
           onTapClose={onMultiChange()}
+          arrowIconColor="#00bb9e"
+          searchIconColor="#00bb9e"
+          toggleIconColor="#00bb9e"
           isMulti
         />
       </Center>
       {selectedInterests &&
         selectedInterests.map(item => <Text>{item.item}</Text>)}
+      <Center>
+        <Button m="16px" w="60%" onPress={submitInterest}>
+          Submit
+        </Button>
+      </Center>
     </Box>
   );
 }
